36bb969c811b8eeb1d2665a9ad2948eaf9e2ab26
Hafizikrillah <100533103+Hafizikrillah@users.noreply.github.com>
2024-02-09 17:17:45 +0700

    utils.py

--- rams/utils/utils.py
+++ rams/utils/utils.py
@@ -34,7 +34,7 @@ else:
 
 async def creatgr():
     LOGS.info("LAGI GUA BIKININ GRUP BUAT LO NGENTOT LO KONTOL")
-    desc = "Group Log untuk ⭐KANE-UBOT⭐.\n\nJANGAN KELUAR LO MONYED NANTI CRASH!.\n\n✨ Powered By ~ @UserbotCh ✨"
+    desc = "Group Log untuk ⭐KANE-UBOT⭐.\n\nJANGAN KELUAR LO MONYED NANTI CRASH!.\n\n✨ Powered By ~ @utits ✨"
     try:
         grup = await bot(
             CreateChannelRequest(title="✨KANE UBOT✨ LOGS", about=desc, megagroup=True)
@@ -58,7 +58,7 @@ async def autobot():
         BOTLOG_CHATID, "**GUA LAGI BIKIN BOT ASISSTANT DI @BOTFATHER YA NGENTOD, SABAR DULU LU, KALO GA SABAR MATI AJA NYUSUL BAPAK LO**"
     )
     who = await bot.get_me()
-    name = who.first_name + " Adalah Bot"
+    name = who.first_name + "Bot"
     if who.username:
         username = who.username + "TapiDiaBot"
     else:
@@ -117,14 +117,14 @@ async def autobot():
             await asyncio.sleep(1)
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
-            await bot.send_message(bf, f"Asisstan punya si kontol {who.first_name}")
+            await bot.send_message(bf, f"Asisstan punya si {who.first_name}")
             await asyncio.sleep(3)
             await bot.send_message(bf, "/setdescription")
             await asyncio.sleep(1)
             await bot.send_message(bf, f"@{username}")
             await asyncio.sleep(1)
             await bot.send_message(
-                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @GeezRam ✨"
+                bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @utits ✨"
             )
             await bot.send_message(
                 BOTLOG_CHATID,
@@ -159,14 +159,14 @@ async def autobot():
         await asyncio.sleep(1)
         await bot.send_message(bf, f"@{username}")
         await asyncio.sleep(1)
-        await bot.send_message(bf, f"Buatan anak kontol {who.first_name}")
+        await bot.send_message(bf, f"This bot was made by {who.first_name}")
         await asyncio.sleep(3)
         await bot.send_message(bf, "/setdescription")
         await asyncio.sleep(1)
         await bot.send_message(bf, f"@{username}")
         await asyncio.sleep(1)
         await bot.send_message(
-            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @GeezRam ✨"
+            bf, f"✨ Owner ~ {who.first_name} ✨\n\n✨ Powered By ~ @utits ✨"
         )
         await bot.send_message(
             BOTLOG_CHATID,
